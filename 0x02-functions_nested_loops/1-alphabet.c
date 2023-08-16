03c9adea7def1161a9b7f548b209cbfff60dec94
verolinka23 <osasumwenveronica@gmail.com>
2023-08-15 18:31:35 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lowercase
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char ve;
+
+	for (ve = 'a'; ve <= 'z'; ve++)
+		_putchar(ve);
+	_putchar('\n');
+}
